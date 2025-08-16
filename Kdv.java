093ffb3ec9628a45c24cd72ccb5364247d3ece19
solisnova <00damlasozen@gmail.com>
2025-08-16 22:50:49 +0300

    Add files via upload

---
+++ Kdv.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+/*Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , 
+tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız. */
+
+public class Kdv {
+    public static void main(String[] args) {
+    Scanner mesaj = new Scanner(System.in);
+    System.out.println("Lütfen satın aldığınız ürünün fiyatını giriniz :");
+    double fiyat = mesaj.nextDouble();
+    mesaj.close();
+
+
+    double sonuc = (fiyat < 1000 && fiyat > 0) ? ((fiyat*18)/100) : ((fiyat*8)/100);
+    System.out.println("satın aldığınız ürünün fiyatı : " + fiyat);
+    System.out.println("KDV tutarınız :" + sonuc);
+    System.out.println("Ürünün KDV'siz fiyatı: " + (fiyat-sonuc));
+
+
+    
+
+
+
+
+
+
+   
+
+  }
+}
